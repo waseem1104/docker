e916e561d74691b3f2ceeb73bce2f6886ef97012
waseem1104 <waseem11@hotmail.fr>
2021-07-20 20:10:25 +0200

    app node

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+// Constants
+const PORT = 3000;
+const HOST = '127.0.0.1';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
